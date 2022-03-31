82331f056658817f0b2dd45aeeb2ab187bfd9d51
bkl <bgx1012@163.com>
2022-03-31 10:29:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# leaflet-polygon-fillPattern-canvas
+
+Extend the Polygon Object to fill canvas element with an image pattern.
+Performance is better than svg rendering.
+Can improve the rendering performance of big data.
+
+### Usage example
+
+```javascript
+var map = L.map("map", {
+  preferCanvas: true,
+}).setView([23.7, 121], 8);
+
+var poly1 = [
+  [24, 121],
+  [24.5, 121],
+  [25.5, 121],
+  [25.5, 124],
+  [24.5, 121.9],
+  [24, 121.9],
+];
+L.polygon(poly1, { imgId: "lamp" }).addTo(map);
+```
+
+![geojson](./examples/geo.jpg)
---
+++ leaflet-polygon.fillPattern-canvas.js
@@ -0,0 +1,50 @@
+/**
+ *  Extend the Polygon Object to set an image to fill the path in SVG
+ *  exp:
+ *    L.Polygon(points, {fill: 'url(image.gif)'});
+ *  Author: lanwei@cloudybay.com.tw
+ */
+
+(function (window, document, undefined) {
+    if (L.Canvas) {
+        L.Canvas.include({
+            _fillStroke: function (ctx, layer) {
+
+                var options = layer.options
+
+                if (options.fill) {
+                    ctx.globalAlpha = options.fillOpacity
+                    ctx.fillStyle = options.fillColor || options.color
+
+                    ctx.fill(options.fillRule || 'evenodd')
+                }
+
+                if (options.stroke && options.weight !== 0) {
+                    if (ctx.setLineDash) {
+                        ctx.setLineDash(layer.options && layer.options._dashArray || [])
+                    }
+
+                    ctx.globalAlpha = options.opacity
+                    ctx.lineWidth = options.weight
+                    ctx.strokeStyle = options.color
+                    ctx.lineCap = options.lineCap
+                    ctx.lineJoin = options.lineJoin
+                    ctx.stroke()
+                    if (options.imgId) {
+
+                        var img = document.getElementById(options.imgId)
+                        ctx.save() // so we can remove the clipping
+                        ctx.clip()
+                        var bounds = layer._rawPxBounds
+                        var size = bounds.getSize()
+                        var pattern = ctx.createPattern(img, 'repeat')
+                        ctx.fillStyle = pattern
+                        ctx.fillRect(bounds.min.x, bounds.min.y, size.x, size.y)
+                        ctx.restore()
+                    }
+                }
+            }
+        })
+    }
+}(this, document))
+
